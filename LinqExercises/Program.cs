388b45b5f2c3c923b096b0a02b726761d00b0392
Anatoli Dimitrov Dvoyachki <253651@VIAUC.DK>
2019-02-21 20:49:10 +0100

    fixed

--- LinqExercises/Program.cs
+++ LinqExercises/Program.cs
@@ -18,46 +18,50 @@ namespace LinqExercises
         private static void Exercise1()
         {
             var names = new[] { "Kim", "Kurt", "Karsten", "Klaus", "Kay" };
-            var withoutK = names.Select(name => name.Replace("K", "C"));
-            Console.WriteLine(string.Join("\n", withoutK));
+            var queryResult = names.Select(name => name.Replace("K", "C"));
+            Console.WriteLine(string.Join("\n", queryResult));
             Console.WriteLine();
         }
 
         private static void Exercise2()
         {
             var names = new[] { "Kim", "Kurt", "Karsten", "Klaus", "Kay" };
-            var withoutVowels = names.Select(name => Regex.Replace(name, "[aeiou]\\B", ""));
-            Console.WriteLine(string.Join("\n", withoutVowels));
+            var queryResult = names.Select(name => Regex.Replace(name, "[aeiou]\\B", string.Empty, RegexOptions.IgnoreCase));
+            Console.WriteLine(string.Join("\n", queryResult));
             Console.WriteLine();
         }
 
         private static void Exercise3()
         {
             var numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            var multipliedBy5 = numbers.Select(num => num * 5);
-            Console.WriteLine(string.Join("\n", multipliedBy5));
+            var queryResult = numbers.Select(num => num * 5);
+            Console.WriteLine(string.Join("\n", queryResult));
             Console.WriteLine();
         }
 
         private static void Exercise4()
         {
+            const int groupSize = 3;
+
             var letters = new[] { "a", "b", "c", "d", "e", "f", "g", "h"};
             
             // Calculate number of groups
-            var numOfGroups = letters.Length / 3;
-            if (letters.Length % 3 > 0)
+            var numOfGroups = letters.Length / groupSize;
+            if (letters.Length % groupSize > 0)
             {
                 numOfGroups++;
             }
 
             var groups = new List<string[]>(numOfGroups);
 
+            // Split in groups
             for (int i = 0; i < numOfGroups; i++)
             {
-                var group = letters.Skip(3 * i).Take(3).ToArray();
+                string[] group = letters.Skip(groupSize * i).Take(groupSize).ToArray();
                 groups.Add(group);
             }
             
+            // Print out
             foreach (string[] group in groups)
             {
                 Console.WriteLine(string.Join(" ", group));
